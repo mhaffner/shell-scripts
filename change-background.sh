5bc8f66301ce2b1b2b44b5ac010b84577a715c59
Matthew Haffner <haffner.matthew.m@gmail.com>
2017-10-13 10:55:41 -0500

    Make modular

--- change-background.sh
+++ change-background.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-feh --randomize --bg-center ~/Sync/pictures/backgrounds/2560x1080/space/*
+
+if [[ "$(hostname)" = "panopticon" ]]; then
+    feh --randomize --bg-center ~/Sync/pictures/backgrounds/2560x1080/space/* ; else
+    feh --randomize --bg-center ~/Sync/pictures/backgrounds/1920x1080/space/*
+fi
