d316bac30c8fdc2734a4d769e928ba5dc43b4f66
Matthew Haffner <haffner.matthew.m@gmail.com>
2017-10-13 11:03:08 -0500

    Add new file

---
+++ convert-webm-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# script taken from here:
+# http://bytefreaks.net/gnulinux/bash/ffmpeg-extract-audio-from-webm-to-mp3
+
+for FILE in *.webm; do
+    echo -e "Processing video '\e[32m$FILE\e[0m'";
+    ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
+done;
+
+rm *.webm
