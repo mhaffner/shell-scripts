8a75904395f9facc25d909b8bfbb579fbf08efe1
Matt Haffner <haffner.matthew.m@gmail.com>
2018-02-23 04:23:01 -0600

    Change system-name to 960-evo

--- change-background.sh
+++ change-background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$(hostname)" = "panopticon" ]]; then
+if [[ "$(hostname)" = "960-evo" ]]; then
     feh --randomize --bg-center ~/Sync/pictures/backgrounds/2560x1080/space/* ; else
     feh --randomize --bg-center ~/Sync/pictures/backgrounds/1920x1080/space/*
 fi
